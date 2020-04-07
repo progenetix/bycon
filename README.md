aa70809468c8f124e539dfcce239f560836cecd1
Michael Baudis <m@baud.is>
2020-04-07 17:26:24 +0200

    proper examples

--- README.md
+++ README.md
@@ -38,9 +38,13 @@
 
 ### Usage Examples
 
-* https://progenetix.org/cgi-bin/bycon/cgi/byconplus.py?datasetIds=arraymap,progenetix&assemblyId=GRCh38&includeDatasetResponses=ALL&referenceName=9&variantType=DEL&startMin=17999999&startMax=21975097&endMin=21967753&endMax=26000000&referenceBases=N&filters=icdom-94403
-* https://progenetix.org/cgi-bin/bycon/cgi/byconplus.py?assemblyId=GRCh38&datasetIds=arraymap,progenetix&filters=NCIT:C3326
-* https://progenetix.org/cgi-bin/bycon/cgi/byconplus.py?assemblyId=GRCh38&datasetIds=arraymap,progenetix&assemblyId=GRCh38&includeDatasetResponses=ALL&referenceName=9&variantType=DEL&startMin=17999999&startMax=21975097&endMin=21967753&endMax=26000000&referenceBases=N&filters=icdom-94403&filters=geolat%3A49%2Cgeolong%3A8.69%2Cgeodist%3A2000000&
-* https://progenetix.org/cgi-bin/bycon/cgi/byconplus.py
-* https://progenetix.org/cgi-bin/bycon/cgi/byconplus.py/service-info/
-* https://progenetix.org/cgi-bin/bycon/cgi/byconplus.py?datasetIds=dipg&assemblyId=GRCh38&includeDatasetResponses=ALL&referenceName=17&start=7577120&referenceBases=G&alternateBases=A&filters=icdot-C71.7&
+Below are some usage examples against the [Progenetix resources](http://progenetix.org).
+
+* The standard Beacon+ CNV test call, retrieving samples with a focal deletion in the _CDKNA/B,MTAP_ locus in Glioblastomas
+    - https://progenetix.org/cgi-bin/bycon/cgi/byconplus.py?datasetIds=arraymap,progenetix&assemblyId=GRCh38&includeDatasetResponses=ALL&referenceName=9&variantType=DEL&startMin=18000000&startMax=21975097&endMin=21967753&endMax=26000000&referenceBases=N&filters=icdom-94403
+* A pure filter call, getting the sample numbers for _NCIT:C3326_ (Adrenal Gland Pheochromocytoma):
+    - https://progenetix.org/cgi-bin/bycon/cgi/byconplus.py?assemblyId=GRCh38&datasetIds=arraymap,progenetix&filters=NCIT:C3326
+* Service info for this Beacon:
+    - https://progenetix.org/cgi-bin/bycon/cgi/byconplus.py/service-info/
+* A "classic" _BeaconAlleleRequest_, for brain stem gliomas with a particular mutation in the _EIF4A1_ gene:
+    - https://progenetix.org/cgi-bin/bycon/cgi/byconplus.py?datasetIds=dipg&assemblyId=GRCh38&includeDatasetResponses=ALL&referenceName=17&start=7577120&referenceBases=G&alternateBases=A&filters=icdot-C71.7&
--- cgi/byconplus.py
+++ cgi/byconplus.py
@@ -19,12 +19,12 @@ logging.basicConfig(
     level=logging.ERROR) # INFO ERROR
 
 """
-https://progenetix.test/cgi-bin/bycon/cgi/byconplus.py?datasetIds=arraymap,progenetix&assemblyId=GRCh38&includeDatasetResponses=ALL&referenceName=9&variantType=DEL&startMin=17999999&startMax=21975097&endMin=21967753&endMax=26000000&referenceBases=N&filters=icdom-94403
-https://progenetix.test/cgi-bin/bycon/cgi/byconplus.py?assemblyId=GRCh38&datasetIds=arraymap,progenetix&filters=NCIT:C3326
-https://progenetix.test/cgi-bin/bycon/cgi/byconplus.py?assemblyId=GRCh38&datasetIds=arraymap,progenetix&assemblyId=GRCh38&includeDatasetResponses=ALL&referenceName=9&variantType=DEL&startMin=17999999&startMax=21975097&endMin=21967753&endMax=26000000&referenceBases=N&filters=icdom-94403&filters=geolat%3A49%2Cgeolong%3A8.69%2Cgeodist%3A2000000&
-https://progenetix.test/cgi-bin/bycon/cgi/byconplus.py
-https://progenetix.test/cgi-bin/bycon/cgi/byconplus.py/service-info/
-https://progenetix.test/cgi-bin/bycon/cgi/byconplus.py?datasetIds=dipg&assemblyId=GRCh38&includeDatasetResponses=ALL&referenceName=17&start=7577120&referenceBases=G&alternateBases=A&filters=icdot-C71.7&
+https://progenetix.org/cgi-bin/bycon/cgi/byconplus.py?datasetIds=arraymap,progenetix&assemblyId=GRCh38&includeDatasetResponses=ALL&referenceName=9&variantType=DEL&startMin=17999999&startMax=21975097&endMin=21967753&endMax=26000000&referenceBases=N&filters=icdom-94403
+https://progenetix.org/cgi-bin/bycon/cgi/byconplus.py?assemblyId=GRCh38&datasetIds=arraymap,progenetix&filters=NCIT:C3326
+https://progenetix.org/cgi-bin/bycon/cgi/byconplus.py?assemblyId=GRCh38&datasetIds=arraymap,progenetix&assemblyId=GRCh38&includeDatasetResponses=ALL&referenceName=9&variantType=DEL&startMin=17999999&startMax=21975097&endMin=21967753&endMax=26000000&referenceBases=N&filters=icdom-94403&filters=geolat%3A49%2Cgeolong%3A8.69%2Cgeodist%3A2000000&
+https://progenetix.org/cgi-bin/bycon/cgi/byconplus.py
+https://progenetix.org/cgi-bin/bycon/cgi/byconplus.py/service-info/
+https://progenetix.org/cgi-bin/bycon/cgi/byconplus.py?datasetIds=dipg&assemblyId=GRCh38&includeDatasetResponses=ALL&referenceName=17&start=7577120&referenceBases=G&alternateBases=A&filters=icdot-C71.7&
 """
 
 ################################################################################
