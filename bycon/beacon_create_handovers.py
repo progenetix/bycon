83064948000f33bd4b002df932e993755e7861b8
Michael Baudis <m@baud.is>
2020-06-16 10:51:07 +0200

    Update beacon_create_handovers.py

--- bycon/beacon_create_handovers.py
+++ bycon/beacon_create_handovers.py
@@ -10,8 +10,30 @@ from .cgi_utils import *
 def dataset_response_add_handovers(**byc):
 
     """podmd
+    #### Beacon "Handovers"
 
+    Beacon `handover` objects represent an efficient and - if used properly -
+    privacy protecting mechanism to enable data delivery, in conjunction with
+    Beacon queries.
 
+    The advantages of using the "handover" mechanism lie especially in:
+
+    * the separation of the standard Beacon variant query and response with the privvacy-protecting
+    response, limited to yes/no/count, from meta-information about individuals and
+    biosamples
+    * the power to link to any type of data delivery protocol for "documents"
+    (i.e. database records) related to the Beacon query matches
+
+    Handled in a controled/protected environment one could in pprinciple use e.g.
+    the `handover` protocol to access the electronic health records (EHR) of patients
+    for which a specific genomic variantr query resulte din a match, to follow up 
+    on thiose patients' history & progress, or to initiate interventions based on
+    accuing evidence related to the matched variant(s).
+
+    While the `handover` mechanisms is powerful through its flexibility, an obvious
+    disadvantage lies in the lack of control about the implemented mechanisms. A
+    good siolution here would be to provide some prototype designs, e.g. "Phenopackets"
+    formatted delivery of "bio" data.s
 
     podmd"""
 
@@ -128,7 +150,7 @@ def _write_variants_bedfile(h_o, **byc):
        
     ds_id = h_o["source_db"]
     accessid = h_o["id"]
-    bed_file =  path.join( byc["config"][ "paths" ][ "out" ], h_o["id"]+'.bed' )
+    bed_file = path.join( byc["config"][ "paths" ][ "out" ], h_o["id"]+'.bed' )
 
     vs = { "DUP": [ ], "DEL": [ ], "SNV": [ ]}
 
