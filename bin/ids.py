53dfc351da5c5f7ff5f4c7e5a41526d9dca0fcee
Michael Baudis <m@baud.is>
2020-10-04 11:28:17 +0200

    ids service example links

--- bin/ids.py
+++ bin/ids.py
@@ -16,7 +16,8 @@ website endpoints. There is no check if the id exists; this is left to the web
 page handling itself.
 
 Stacking with the "pgx:" prefix is allowed.
-
+* <https://progenetix.org/services/ids/pgxbs-kftva5zv>
+* <https://progenetix.org/services/ids/PMID:28966033>
 * <https://progenetix.org/services/ids/NCIT:C3262>
 podmd"""
 
