54b2771682f7a39641e2e5df65ae701f5953c1bd
Bo Gao <kaye_gao@hotmail.com>
2020-10-05 11:10:21 +0200

    dynamic loading

--- bin/services.py
+++ bin/services.py
@@ -5,22 +5,24 @@ from os import path as path
 from urllib.parse import urlparse
 import sys, re, cgitb
 
+from importlib import import_module
+
 # local
 dir_path = path.dirname(path.abspath(__file__))
 sys.path.append(path.join(path.abspath(dir_path), '..'))
 
-from bin.biosamples import biosamples
-from bin.byconplus import byconplus
-from bin.collations import collations
-from bin.cytomapper import cytomapper
-from bin.dbstats import dbstats
-from bin.deliveries import deliveries
-from bin.genespans import genespans
-from bin.geolocations import geolocations
-from bin.ids import ids
-from bin.ontologymaps import ontologymaps
-from bin.phenopackets import phenopackets
-from bin.publications import publications
+# from bin.biosamples import biosamples
+# from bin.byconplus import byconplus
+# from bin.collations import collations
+# from bin.cytomapper import cytomapper
+# from bin.dbstats import dbstats
+# from bin.deliveries import deliveries
+# from bin.genespans import genespans
+# from bin.geolocations import geolocations
+# from bin.ids import ids
+# from bin.ontologymaps import ontologymaps
+# from bin.phenopackets import phenopackets
+# from bin.publications import publications
 
 """podmd
 The `services` application deparses a request URI and calls the respective
@@ -43,21 +45,21 @@ podmd"""
 ################################################################################
 
 def main():
-    services = {
-        "biosamples": biosamples,
-        "byconplus": byconplus,
-        "cytomapper": cytomapper,
-        "dbstats": dbstats,
-        "deliveries": deliveries,
-        "geolocations": geolocations,
-        "ids": ids,
-        "publications": publications,
-        "genespans": genespans,
-        "ontologymaps": ontologymaps,
-        "phenopackets": phenopackets,
-        "collations": collations
-    }
 
+    services = [
+        "biosamples",
+        "byconplus",
+        "cytomapper",
+        "dbstats",
+        "deliveries",
+        "geolocations",
+        "ids",
+        "publications",
+        "genespans",
+        "ontologymaps",
+        "phenopackets",
+        "collations"
+    ]
 
     if "debug=1" in environ.get('REQUEST_URI'):
         cgitb.enable()
@@ -73,10 +75,17 @@ def main():
         if len(p_items) > i:
             i += 1
             if p == "services":
-                if p_items[ i ] in services.keys():    
+                if p_items[ i ] in services:    
                     f = p_items[ i ]
-                    # TODO: dynamic loader, then call
-                    services[f](f)
+                    
+                    # dynamic package/function loading
+                    try:
+                        mod = import_module(f)
+                        serv = getattr(mod, f)
+                        serv(f)
+                    except Exception as e:
+                        print('Service name error: {}'.format(e))
+
                     exit()
 
     print('Content-Type: text')
