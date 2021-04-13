ed50ebfd6c7b1c5fbaf8e4e14007e102e80afd09
mbaudis <mbaudis@me.com>
2021-04-13 14:33:20 +0200

    Update publication_utils.py

--- byconeer/lib/publication_utils.py
+++ byconeer/lib/publication_utils.py
@@ -7,12 +7,16 @@ import csv
 ##############################################################################
  
 def create_short_publication_label(author="", title="", year=""):
-    
-    short_author = re.sub(r'(.{2,16}[\w\.\-]+? \w\-?\w?\w?)(\,| and ).*?$', r'\1 et al.', author)
 
-    if len(title) <= 50:
+    authors_max_characters = 16
+    title_max_words = 9
+    title_max_length = 60
+    
+    short_author = re.sub(r'(.{2,'+str(authors_max_characters)+'}[\w\.\-]+? \w\-?\w?\w?)(\,| and ).*?$', r'\1 et al.', author)
+
+    if len(title) <= title_max_length:
         label = short_author + f' ({year}) ' + title
     else:
-        label = short_author + f' ({year}): ' + ' '.join(title.split(' ')[:9]) + '...'
+        label = short_author + f' ({year}): ' + ' '.join(title.split(' ')[:title_max_words]) + '...'
         
     return label
