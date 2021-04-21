fa8bd56da0f0c524b4ea519b53167bbaca5d8f58
Sofia Pfund <sofia.pfund@uzh.ch>
2021-04-21 13:36:12 +0200

    Update pubUpdater.py
    Added argparse() to specify file path

--- byconeer/pubUpdater.py
+++ byconeer/pubUpdater.py
@@ -1,17 +1,22 @@
 from os import path
 from pymongo import MongoClient
-from byconeer.lib.publication_utils import (read_annotation_table, create_progenetix_posts) ### ?
+from byconeer.lib.publication_utils import (read_annotation_table, create_progenetix_posts) ### do I import them like this?
 
 """
 ## `publicationUpdater`
 
 """
 
-dir_path = path.dirname( path.abspath(__file__) ) ### prints the path of the directory where file is found
-file_path = path.join(dir_path, 'annotation_table.txt') ### name of your annotation file (! tab-separated values !)
+##############################################################################
+##############################################################################
+##############################################################################
 
-##############################################################################
-##############################################################################
+def _get_args():
+    parser = argparse.ArgumentParser(description="Read publication annotations, create MongoDB posts and update the database.")
+    parser.add_argument("-f", "--filepath", help="Path of the .tsv file containing the annotations on the publicationßs.", type=str)
+    args = parser.parse_args()
+    return args
+
 ##############################################################################
 
 def main():
@@ -20,28 +25,30 @@ def main():
 ##############################################################################
 
 def update_publications():
-    
+
+    file_path = _get_args()
+
     # Read annotation table:
     rows = read_annotation_table(file_path)
-    
+
     # Use retrieval to generate posts to be uploaded on MongoDB
     posts = create_progenetix_posts(rows)
-    
+
     # Connect to MongoDB and load publication collection
-    client = MongoClient() 
+    client = MongoClient()
     cl = client['progenetix'].publications
     ids = cl.distinct("id")
-    
+
     # Update the database
-    for post in posts:   
-            
+    for post in posts:
+
         if post["id"] in ids:
             print(post["id"], ": this article is already on the progenetix publications collection.")
-       
+
         else:
             print(post["id"], ": this article isn't on the progenetix publications collection yet.")
             result = cl.insert_one(post)
-            result.inserted_id  
+            result.inserted_id
 
 ##############################################################################
 
