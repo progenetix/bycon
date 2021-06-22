1b25849f15676aba56bfd7302198a3051646b78e
mbaudis <mbaudis@me.com>
2021-06-22 13:34:05 +0200

    TNM for biosamplesRefresher.py (and more)

--- byconeer/biosamplesRefresher.py
+++ byconeer/biosamplesRefresher.py
@@ -21,6 +21,8 @@ from beaconServer.lib import *
 
 ## `biosamplesRefresher`
 
+* `biosamplesRefresher.py -d progenetix -m stats`
+
 """
 
 ################################################################################
@@ -34,7 +36,7 @@ def _get_args(byc):
     parser.add_argument("-a", "--alldatasets", action='store_true', help="process all datasets")
     parser.add_argument("-f", "--filters", help="prefixed filter values, comma concatenated")
     parser.add_argument("-t", "--test", help="test setting")
-    parser.add_argument('-i', '--inputfile', help='accustom file  to specify input data')
+    parser.add_argument('-i', '--inputfile', help='a custom file to specify input data')
     parser.add_argument('-m', '--mode', help='update modus')
     byc.update({ "args": parser.parse_args() })
 
@@ -106,58 +108,13 @@ def _process_dataset(ds_id, pub_labels, byc):
 
     bar = Bar("{} samples from {}".format(no, ds_id), max = no, suffix='%(percent)d%%'+" of "+str(no) )
 
+    count = 0
     for bsid in bs_ids:
 
         s = bios_coll.find_one({ "id":bsid })
+        update_obj = {}
 
-        """
-        The following code will refresh callset ids and their statistics into
-        the biosamples entries.
-        If no callsets are found this will result in empty attributes; if
-        more than one callset is found the average of the CNV statistics will be used.
-        """
-        cs_ids = [ ]
-        cs_stats_no = 0
-        cnv_stats = { }
-        cnvstatistics = {k:[] for k in byc["these_prefs"]["refreshing"]["cnvstatistics"]}
-        cs_query = { "biosample_id": s["id"] }
-
-        cs_ids = cs_coll.distinct( "id", cs_query )
-
-        if len(cs_ids) < 1:
-            print("\n!!! biosample {} had no callset !!!".format(s["id"]))
-            continue
-
-        for cs in cs_coll.find( cs_query ):
-            cs_ids.append(cs["id"])
-
-            if "cnvstatistics" in cs["info"]:
-                cs_stats_no = cs_stats_no + 1
-                for s_k in cnvstatistics.keys():
-                    if s_k in cs["info"]["cnvstatistics"]:
-                        cnvstatistics[ s_k ].append(cs["info"]["cnvstatistics"][ s_k ])
-        any_stats = False
-        if cs_stats_no > 0:
-            for s_k in cnvstatistics.keys():
-                n = len(cnvstatistics[ s_k ])
-                if n > 0:
-                    any_stats = True
-                    cnv_stats[ s_k ] = sum(cnvstatistics[ s_k ]) / n
-                    if cnv_stats[ s_k ] < 1:
-                        cnv_stats[ s_k ] = round( cnv_stats[ s_k ], 3)
-                    else:
-                        cnv_stats[ s_k ] = int( cnv_stats[ s_k ] )
-        else:
-            no_cs_no += 1
-
-        if not any_stats:
-            no_stats_no += 1
-
-        update_obj = { "info.callset_ids": cs_ids, "info.cnvstatistics": cnv_stats }
-
-        """
-        --- other biosample modification code
-        """
+        _update_cnv_stats(cs_coll, bsid, update_obj, byc)
 
         if "sampledTissue" in s:
             if "UBERON" in s["sampledTissue"]["id"]:
@@ -181,6 +138,23 @@ def _process_dataset(ds_id, pub_labels, byc):
                 e_r_u.append(e_r)                   
             update_obj.update( { "external_references": e_r_u } ) 
 
+        # if "pathological_tnm_findings" in s:
+        #     update_obj.update( { "pathological_tnm_findings": s["pathological_tnm_findings"] } )
+        # else:    
+        # TODO: check existing content first  
+        update_obj.update( { "pathological_tnm_findings": [] } )
+        if "info" in s:
+            if "tnm" in s["info"]:
+                if not isinstance(s["info"]["tnm"], str):
+                    continue
+                for tnm_def in byc["these_prefs"]["pathologicalTnmFindings"].values():
+                    if re.compile( tnm_def["pattern"] ).match( s["info"]["tnm"] ):
+                        update_obj["pathological_tnm_findings"].append({
+                            "id": tnm_def["id"],
+                            "label": tnm_def["label"]
+                        })
+                        count += 1
+
         ####################################################################
 
         if not byc["args"].test:
@@ -190,8 +164,7 @@ def _process_dataset(ds_id, pub_labels, byc):
 
     bar.finish()
 
-    print("{} {} biosamples had no callsets".format(no_cs_no, ds_id))
-    print("{} {} biosamples received no CNV statistics".format(no_stats_no, ds_id))         
+    print("TNMs: {}".format(count))
 
 ################################################################################
 
@@ -205,6 +178,56 @@ def _map_publication_labels(byc):
         pub_labels.update( { pub["id"] : pub["label"] } )
 
     return pub_labels
+
+################################################################################
+
+def _update_cnv_stats(cs_coll, bsid, update_obj, byc):
+
+    if not byc["args"].mode:
+        return update_obj
+    if not "stat" in byc["args"].mode:
+        return update_obj
+
+    """
+    The following code will refresh callset ids and their statistics into
+    the biosamples entries.
+    If no callsets are found this will result in empty attributes; if
+    more than one callset is found the average of the CNV statistics will be used.
+    """
+    cs_ids = [ ]
+    cs_stats_no = 0
+    cnv_stats = { }
+    cnvstatistics = {k:[] for k in byc["these_prefs"]["refreshing"]["cnvstatistics"]}
+    cs_query = { "biosample_id": bsid }
+
+    cs_ids = cs_coll.distinct( "id", cs_query )
+
+    if len(cs_ids) < 1:
+        print("\n!!! biosample {} had no callset !!!".format(s["id"]))
+        return update_obj
+
+    for cs in cs_coll.find( cs_query ):
+        cs_ids.append(cs["id"])
+
+        if "cnvstatistics" in cs["info"]:
+            cs_stats_no = cs_stats_no + 1
+            for s_k in cnvstatistics.keys():
+                if s_k in cs["info"]["cnvstatistics"]:
+                    cnvstatistics[ s_k ].append(cs["info"]["cnvstatistics"][ s_k ])
+
+    if cs_stats_no > 0:
+        for s_k in cnvstatistics.keys():
+            n = len(cnvstatistics[ s_k ])
+            if n > 0:
+                cnv_stats[ s_k ] = sum(cnvstatistics[ s_k ]) / n
+                if cnv_stats[ s_k ] < 1:
+                    cnv_stats[ s_k ] = round( cnv_stats[ s_k ], 3)
+                else:
+                    cnv_stats[ s_k ] = int( cnv_stats[ s_k ] )
+
+    update_obj.update( { "info.callset_ids": cs_ids, "info.cnvstatistics": cnv_stats } )
+
+    return update_obj
 
 ################################################################################
 ################################################################################
