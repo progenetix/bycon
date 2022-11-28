d1d85be06bb00edbfc0d2fa0b604f9dc8ac18ca7
Michael Baudis <mbaudis@me.com>
2022-11-28 11:54:56 +0100

    aggregator ...

--- beaconServer/aggregator.py
+++ beaconServer/aggregator.py
@@ -1,6 +1,6 @@
 #!/usr/local/bin/python3
 
-import re, json, sys, datetime, argparse
+import re, json, sys, datetime, argparse, urllib.parse
 from os import path, environ, pardir
 from copy import deepcopy
 from liftover import get_lifter
@@ -25,20 +25,32 @@ Queries are expected in the GRCh38 Beacon v2 format with parameters:
 * `alternateBases`
 * `referenceBases` aren't supported by most olf beacons?
 
+TODO:
+
+* move from the "parameters per instance" to "parameters per version"
+    - this may not work if those instances take "liberties" with their parameters ...
+* split into different functions
+    - a main aggregator which handles i/o for a list of beacons (i.e. the original)
+    - an option to have it just return the query URLs w/o executing them, e.g. as handovers
+    - a single query processor, taking the modified query url as input, retrieving the
+      query & providing the response in the standard v2 format
+* provide a prototype front-end which parses the list of transformed queries & uses
+  the retriever to asynchronously populate the front-end, and to provide an aggregate
+  summary
+* message if query (e.g. brackets ...) is not supported
+* liftover examples ... https://pypi.org/project/liftover/ 
+
+
+#### Tests
 
 * https://progenetix.org/services/intervalFrequencies/?datasetIds=progenetix&filters=NCIT:C7376,PMID:22824167
 * https://progenetix.org/services/intervalFrequencies/?datasetIds=progenetix&id=pgxcohort-TCGAcancers
 * https://progenetix.org/cgi/bycon/services/intervalFrequencies.py/?output=pgxseg&datasetIds=progenetix&filters=NCIT:C7376
+* http://progenetix.org/beacon/aggregator/?requestedGranularity=boolean&referenceBases=G&alternateBases=A&limit=1000&skip=0&datasetIds=progenetix&assemblyId=GRCh38&referenceName=refseq%3ANC_000017.11&filterLogic=AND&includeDescendantTerms=True&start=7577120
 
-https://cancer.sanger.ac.uk/api/ga4gh/beacon?allele=A&chrom=7&dataset=cosmic&format=json&pos=140753336&ref=38
-http://progenetix.test/services/aggregator/debug=1
-http://progenetix.test/cgi/bycon/beaconServer/aggregator.py?debug=1&reference_name=7&start=140753335&alternate_bases=A&assemblyId=GRCh38&responseEntityId=genomicVariant
-
-
-#### TODO
-
-* message if query (e.g. brackets ...) is not supported
-* liftover examples ... https://pypi.org/project/liftover/ 
+* https://cancer.sanger.ac.uk/api/ga4gh/beacon?allele=A&chrom=7&dataset=cosmic&format=json&pos=140753336&ref=38
+* http://progenetix.org/services/aggregator/debug=1
+* http://progenetix.org/cgi/bycon/beaconServer/aggregator.py?debug=1&reference_name=7&start=140753335&alternate_bases=A&assemblyId=GRCh38&responseEntityId=genomicVariant
 
 """
 
@@ -70,7 +82,7 @@ def aggregator():
     if "selected_beacons" in byc["form_data"]:
         byc["service_config"].update({"selected_beacons": byc["form_data"]["selected_beacons"]})
 
-    b_p = byc["service_config"]["beacon_params"]["parameters"]
+    b_p = byc["service_config"]["beacon_params"]["instances"]
 
     byc["service_response"]["meta"]["received_request_summary"]["requested_granularity"] ="boolean"
     check_switch_to_boolean_response(byc)
@@ -87,17 +99,19 @@ def aggregator():
         2. form values w/ remap infor are processed
         3. the non-remapped p=v are added
         4. fixed `value` parameters are (over-)written
+        TODO: should we be explicit in parsing selected parameters,
+        i.e. evaluating all (relevant) v2 ones?
         """
 
         _set_default_values(pvs, ext_defs, byc)
         _remap_parameters_values(pvs, ext_defs, byc)
         _add_parameter_values(pvs, ext_defs, byc)
         _set_fixed_values(pvs, ext_defs, byc)
-        _stringify_query(pvs)
 
-        url = "{}{}".format(ext_defs["base_url"], _stringify_query(pvs))
+        url = "{}{}".format(ext_defs["base_url"], urllib.parse.urlencode(pvs))
 
         r = _retrieve_beacon_response(url)
+        # prjsoncam(r)
         r_f = _format_response(r, url, ext_defs, byc)
         byc["service_response"]["response"]["response_sets"].append(r_f)
 
@@ -123,7 +137,9 @@ def _remap_parameters_values(pvs, ext_defs, byc):
 
     v_rs_chros = byc["variant_definitions"]["chro_aliases"]
     v_p_defs = byc["variant_definitions"]["parameters"]
-    form_p = deepcopy(byc["variant_pars"])
+    form_p = deepcopy(byc["form_data"])
+
+    target_assembly = ext_defs["parameter_map"].get("assembly_id", "GRCh38")
 
     for v_p_k in v_p_defs.keys():
         if not v_p_k in form_p.keys():
@@ -140,7 +156,11 @@ def _remap_parameters_values(pvs, ext_defs, byc):
                         val = v_rs_chros[val]
 
             elif "start" in v_p_k or "end" in v_p_k:
-                val[0] += int(v_p_v.get("shift", 0))
+                val[0] = int(val[0]) + int(v_p_v.get("shift", 0))
+                
+                if not "38" in target_assembly:
+                    _lift_positions(target_assembly, val)
+
             if "array" in v_p_defs[v_p_k].get("type", "string"):
                 val = ",".join(map(str, val))
 
@@ -150,9 +170,18 @@ def _remap_parameters_values(pvs, ext_defs, byc):
 
 ################################################################################
 
+def _lift_positions(target_assembly, val):
+
+    # TODO: liftover
+
+    return val
+
+################################################################################
+
 def _add_parameter_values(pvs, ext_defs, byc):
 
-    form_p = deepcopy(byc["variant_pars"])
+    # form_p = deepcopy(byc["variant_pars"])
+    form_p = deepcopy(byc["form_data"])
     v_p_defs = byc["variant_definitions"]["parameters"]
     
     for v_p_k in v_p_defs.keys():
@@ -181,16 +210,10 @@ def _set_fixed_values(pvs, ext_defs, byc):
 
 ################################################################################
 
-def _stringify_query(pvs):
-
-    return "&".join("{}={}".format(k, v) for k, v in pvs.items())
-
-################################################################################
-
 def _retrieve_beacon_response(url):
 
     try:
-        return requests.get(url, timeout=byc["service_config"]["request_timeout"]).json()
+        return requests.get(url, headers={ "Content-Type" : "application/json"}, timeout=byc["service_config"]["request_timeout"]).json()
     except requests.exceptions.RequestException as e:
         return { "error": e }
 
@@ -202,13 +225,19 @@ def _format_response(r, url, ext_defs, byc):
     if "response_summary" in ext_defs["response_map"].keys():
         summary_k = ext_defs["response_map"]["response_summary"].get("remap", summary_k)
 
-    b_resp = r.get(summary_k, {})
+    if "_root_" in summary_k:
+        b_resp = r
+    else:
+        b_resp = r.get(summary_k, {})
+
     r = {
             "id": ext_defs["id"],
             "api_version": ext_defs.get("api_version", None),
             "exists": test_truthy(b_resp.get("exists", False)),
             "info": {
-                "query_url": url
+                "query_url": urllib.parse.unquote(url),
+                "welcome_url": ext_defs.get("welcome_url", None),
+                "logo_url": ext_defs.get("logo_url", None)
             },
             "error": r.get("error", None)
         }
--- lib/cgi_parsing.py
+++ lib/cgi_parsing.py
@@ -335,6 +335,10 @@ def cgi_print_response(byc, status_code):
     # with simple key-value pairs)
     # TODO: universal text table converter ... partially implemented
 
+    # print(byc["output"])
+    # print(byc["service_response"]["response"])
+
+
     if "text" in byc["output"]:
 
         cgi_simplify_response(byc)
