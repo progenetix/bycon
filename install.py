88c3b1d3ac0c6d6b672f35c636cfce7ee677165b
Michael Baudis <mbaudis@me.com>
2023-02-06 09:52:47 +0100

    install script and config

--- install.py
+++ install.py
@@ -1,7 +1,10 @@
 #!/usr/local/bin/python3
 
-import sys, yaml
+import sys
 from os import path, system
+
+from ruamel.yaml.main import round_trip_load, round_trip_dump
+from ruamel.yaml.comments import CommentedMap
 
 dir_path = path.dirname( path.abspath(__file__) )
 pkg_path = path.join( dir_path, "bycon" )
@@ -27,30 +30,51 @@ def main():
 
 def install():
 
-    c_f = path.join( pkg_path, "config", "config.yaml" )
+    i_f = path.join( dir_path, "install.yaml" )
 
     try:
-        with open( c_f ) as y_c:
-            config = yaml.load( y_c , Loader=yaml.FullLoader)
+        with open( i_f ) as y_c:
+            install = round_trip_load( y_c )
     except Exception as e:
         print(e)
         exit()
 
     # WARNING: This assumes that the values are sensible...
     for p in ["system_user", "system_group", "bycon_install_dir", "web_temp_dir"]:
-        p_v = config.get(p, None)
+        p_v = install.get(p, None)
         if p_v is None:
-            print("¡¡¡ No `{}` value defined in {} !!!".format(p, c_f))
+            print("¡¡¡ No `{}` value defined in {} !!!".format(p, i_f))
             exit()
 
     for d in ["bycon_install_dir", "web_temp_dir"]:
-        d_p = path.join( *config.get(d, ["___path-error___"]) )
+        d_p = path.join( *install.get(d, ["___path-error___"]) )
         if not path.isdir(d_p):
             print("¡¡¡ {}: {} does not exist - please check & create !!!".format(d, d_p))
             exit()
 
-    b_i_d_p = path.join( *config["bycon_install_dir"] )
-    w_t_d_p = path.join( *config["web_temp_dir"] )
+    b_i_d_p = path.join( *install["bycon_install_dir"] )
+    w_t_d_p = path.join( *install["web_temp_dir"] )
+
+    # this block modifies the main config file, so that the `web_temp_dir` is
+    # in line
+
+    c_f = path.join( pkg_path, "config", "config.yaml" )
+    c_f_bck = path.join( pkg_path, "config", "config.yaml.bck" )
+    try:
+        with open( c_f ) as y_c:
+            config = round_trip_load( y_c )
+        system('cp {} {}'.format(c_f, c_f_bck))
+    except Exception as e:
+        print(e)
+        exit()
+
+    config.update({ "web_temp_dir":install["web_temp_dir"]})
+    cf_p = CommentedMap(config)
+
+    with open(c_f, 'w') as out_f:
+        round_trip_dump(cf_p, out_f)
+
+    # / config modification
 
     excludes = [
         "__pycache__",
@@ -61,10 +85,11 @@ def install():
     e_s = '--exclude="{}"'.format('" --exclude="'.join(excludes))
  
     system('sudo rsync -av {} {}/ {}/'.format(e_s, pkg_path, b_i_d_p))
-    system('sudo chown -R {}:{} {}'.format(config["system_user"], config["system_group"], b_i_d_p))
+    system('sudo chown -R {}:{} {}'.format(install["system_user"], install["system_group"], b_i_d_p))
     system('sudo chmod 775 {}/beaconServer/*.py'.format(b_i_d_p))
     system('sudo chmod 775 {}/services/*.py'.format(b_i_d_p))
     system('sudo chmod -R 1777 {}'.format(w_t_d_p))
+    print("Updated the `web_temp_dir` in {} to\n{}".format(c_f, w_t_d_p))
     print("Updated bycon files from\n{}\nto\n{}".format(pkg_path, b_i_d_p))
 
     exit()
