07b629499fb121be007b74411a29232321a6e096
Michael Baudis <m@baud.is>
2024-12-09 14:38:20 +0100

    2.1.0

--- beaconServer/README.md
+++
@@ -1,4 +0,0 @@
-## `bycon/beaconServer` directory
-
-This directory contains the Beacon app, calling the individual v2
-endpoints.
--- importers/analysesUpdater.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-from bycon import byconServiceLibs
-from importer_helpers import ByconautImporter
-
-BI = ByconautImporter()
-BI.update_analyses()
