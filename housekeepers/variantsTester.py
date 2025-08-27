81b7fc037bd69c3224987fc981c638d36afed661
Michael Baudis <m@baud.is>
2025-08-27 16:24:51 +0200

    Update variantsTester.py

--- housekeepers/variantsTester.py
+++ housekeepers/variantsTester.py
@@ -9,8 +9,11 @@ from byconServiceLibs import import_datatable_dict_line
 
 BI = ByconImporter()
 BV = ByconVariant()
+BI.get_input_file()
 variants, fieldnames = BI.read_records("genomicVariant")
+i = 0
 for v in variants:
+	i += 1
 	prjsonnice(v)
 	f_v = import_datatable_dict_line({"updated": datetime.now().isoformat()}, fieldnames, v, "genomicVariant")
 	prjsonnice(f_v)
@@ -18,3 +21,5 @@ for v in variants:
 	print('\n################ VRSified #########')
 	prjsonnice(vrs_v)
 	print('\n############### / VRSified ########')
+	if BYC["TEST_MODE"] and i > BYC_PARS.get("test_mode_count"):
+		break
