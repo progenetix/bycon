02591681b65d75d228c64a2cccd36038955d8897
Michael Baudis <m@baud.is>
2025-11-17 16:16:59 +0100

    Update stats.js

--- beaconplusWeb/src/pages/stats.js
+++ beaconplusWeb/src/pages/stats.js
@@ -4,17 +4,26 @@ import { Layout } from "./../site-specific/Layout"
 import { withUrlQuery } from "../hooks/url-query"
 import { urlRetrieveIds } from "./../hooks/api"
 
+// http://beaconplus.org/stats/?datasetIds=progenetix&ageSplits=P0D,P1Y,P2Y,P18Y,P21Y,P40Y
+
 const StatsPage = withUrlQuery(({ urlQuery }) => {
-  var { id, datasetIds } = urlRetrieveIds(urlQuery)
+  var { datasetIds } = urlRetrieveIds(urlQuery)
   console.log(datasetIds)
 
   var ageSplits = urlQuery["ageSplits"]
 
-  console.log(id)
-
   const title = `${datasetIds} Data Content Overview`
+  const leadText = `This page shows some data statistics for the ${datasetIds}
+dataset. Current modification options are through URL parameters for (with examples):
+
+* "datasetIds=progenetix" (or other supported dataset)
+* "ageSplits=P0D,P1Y,P2Y,P18Y,P21Y,P40Y" ... as example for age binning
+* "followupTime=P0D,P1D,P6M,P1Y,P5Y,P10Y" ... as example for followup binning
+
+Please allow for some loading time.`
+
   return (
-    <Layout title={title} headline={title}>
+    <Layout title={title} headline={title} leadPanelMarkdown={leadText}>
       <DatasetStats dataset_id={datasetIds} age_splits={ageSplits} />
     </Layout>
   )
