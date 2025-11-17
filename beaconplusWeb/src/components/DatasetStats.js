68b30c974e9144467b181783925a11195a911fb5
Michael Baudis <m@baud.is>
2025-11-17 16:09:59 +0100

    stats page

--- beaconplusWeb/src/components/DatasetStats.js
+++ beaconplusWeb/src/components/DatasetStats.js
@@ -5,8 +5,8 @@ import {
   useProgenetixApi,
 } from "./../hooks/api"
 
-export default function DatasetStats({dataset_id}) {
-  const summaryURL = `${basePath}beacon/datasets/${dataset_id}`
+export default function DatasetStats({dataset_id, age_splits}) {
+  const summaryURL = `${basePath}beacon/datasets/${dataset_id}?ageSplits=${age_splits}`
   const summaryReply = useProgenetixApi(summaryURL)
   return (
     <AggregatedPlots
--- beaconplusWeb/src/pages/stats.js
+++ beaconplusWeb/src/pages/stats.js
@@ -1,14 +1,23 @@
 import React from "react"
 import DatasetStats from "./../components/DatasetStats"
 import { Layout } from "./../site-specific/Layout"
-import { DATASETDEFAULT } from "./../hooks/api"
+import { withUrlQuery } from "../hooks/url-query"
+import { urlRetrieveIds } from "./../hooks/api"
 
+const StatsPage = withUrlQuery(({ urlQuery }) => {
+  var { id, datasetIds } = urlRetrieveIds(urlQuery)
+  console.log(datasetIds)
 
-export default function Page() {
-  const title = `${DATASETDEFAULT} Data Content Overview`
+  var ageSplits = urlQuery["ageSplits"]
+
+  console.log(id)
+
+  const title = `${datasetIds} Data Content Overview`
   return (
-    <Layout title={title} headline="">
-      <DatasetStats dataset_id={DATASETDEFAULT} />
+    <Layout title={title} headline={title}>
+      <DatasetStats dataset_id={datasetIds} age_splits={ageSplits} />
     </Layout>
   )
-}
+})
+
+export default StatsPage
