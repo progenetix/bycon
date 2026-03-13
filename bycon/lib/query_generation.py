f4a327147bad11a921d886222c1093fc82d92182
Michael Baudis <m@baud.is>
2026-03-13 15:37:18 +0100

    Update query_generation.py

--- bycon/lib/query_generation.py
+++ bycon/lib/query_generation.py
@@ -494,7 +494,7 @@ class ByconQuery:
                 prdbug(f"!!! no mapping for variant type {v_t} in genemap query!")
                 return False
 
-        if maxl := v_pars.get(p_n):
+        if maxl := v_pars.get("variant_max_length"):
             query_obj.update({"max_segment": {"$lte": maxl}})
 
         if len(query_obj.keys()) > 1:
@@ -503,7 +503,12 @@ class ByconQuery:
             # direct dotted query
             query = {"var_genemaps.gene_symbol": gene}
 
+        print(f"...Genemap query:")
+        prjsonnice(query)
+
         self.__update_queries_for_entity(query, entity)
+
+
 
         # returning True since the query is directly added to the queries list
         # and no further geneId parsing is needed
