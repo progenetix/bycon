09cf410d4e443f20f7f3c941c22fd9ed6e058a10
Michael Baudis <m@baud.is>
2026-03-30 08:12:33 +0200

    remove utilities ... to byconaut & byconeer
    A first clean-out of "non Beacon essential" maintenance & data processing utilities to Codeberg ... byconaut/byconeer.
    
    TODO: housekeeping.py etc.

--- housekeepers/sampletableExporter.py
+++
@@ -1,7 +0,0 @@
-#!/usr/local/bin/python3
-
-from bycon import *
-from byconServiceLibs import ByconDatatableExporter, ByconDatatableExporter
-
-fd = ByconResultSets().get_flattened_data()
-ByconDatatableExporter(fd).export_datatable()
