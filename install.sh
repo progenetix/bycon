59bd40fef0aaedf12a3d026a2f44a89f36c85b9e
Michael Baudis <m@baud.is>
2026-04-30 11:01:19 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+python3 -m build --no-isolation --sdist .
+BY=(./dist/bycon*tar.gz)
+pip3 install $BY --no-index --no-build-isolation --break-system-packages
+rm -rf ./build                           
+rm -rf ./dist
+rm -rf ./src/bycon.egg-info
